d48981681ec961bacd82a214e26593907ac7400c
Destroliedel22 <Jordiwalet@gmail.com>
2025-10-31 11:35:04 +0100

    added movement

--- EquipmentSystem/Assets/_Project/Scripts/Walking.cs
+++ EquipmentSystem/Assets/_Project/Scripts/Walking.cs
@@ -6,6 +6,12 @@ public class Walking : MonoBehaviour
 {
     public InputSystem_Actions PlayerInput;
 
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float drag;
+
+    private Rigidbody m_Rigidbody;
+    private Vector3 movement;
+
     private void Awake()
     {
         PlayerInput = new InputSystem_Actions();
@@ -24,12 +30,25 @@ public class Walking : MonoBehaviour
         PlayerInput.Disable();
     }
 
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_Rigidbody.AddForce(movement * walkSpeed);
+    }
+
     private void OnWalk(InputAction.CallbackContext context)
     {
+        m_Rigidbody.linearDamping = 0;
         Vector2 input = context.ReadValue<Vector2>();
+        movement = new Vector3(input.x, 0, input.y);
     }
     private void OnWalkCanceled(InputAction.CallbackContext context)
     {
-
+        movement = Vector2.zero;
+        m_Rigidbody.linearDamping = drag;
     }
 }
